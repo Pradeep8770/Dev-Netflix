cc0be753a2d2334be67687653d273b90553f9ac8
Pradeep8770 <padeeppatel5148@gamail.com>
2023-02-11 22:57:17 +0530

    adding new jsx files

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Card from "./Card";
+
+const App =()=>{
+    return <>
+    <h1 className="heading"> Top 5 Netflix web series</h1>
+    <div className="cards-page">
+        <Card />
+
+    </div>
+    
+    </>
+}
+
+export default App;
